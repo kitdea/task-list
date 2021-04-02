3c03f38d0cc5da258cf4e3aa12564a6826d0fbb1
kitdea <francismarrosales@gmail.com>
2021-04-02 13:11:25 +0800

    adding script

--- public/app.js
+++ public/app.js
@@ -1,11 +1,30 @@
-const taskInput = document.getElementById('tdl-add-task');
-const addBtn = document.getElementById('tdl-add-btn');
+const form = document.querySelector('#tdl-task-form');
+const taskList = document.querySelector('#tdl-task-list')
+const taskInput = document.querySelector('#tdl-task-input');
+const addBtn = document.querySelector('#tdl-add-btn');
 
-function addTask() {
-    if(taskInput.value == "") {
-        alert("Task Empty!");
-        return;
-    };
+loadEventListeners();
+
+function loadEventListeners() {
+    form.addEventListener('tdl-add-btn', addTask);
 };
 
-console.log(addTask);
+function addTask() {
+    if(taskInput.value === "") {
+        alert("Add a Task!");
+    }
+
+    const li = document.createElement('li');
+    li.className = 'tdl-task-list-item';
+    li.appendChild(document.createTextNode(taskInput.value));
+
+    const link = document.createElement('a');
+    link.className = 'delete-item';
+    link.innerHTML = '<i class="fas fa-trash-alt delete-icon flex-shrink-0 h-6 w-6 text-red-400"></i>';
+    li.appendChild(link);
+
+    
+
+    e.preventDefault();
+};
+
