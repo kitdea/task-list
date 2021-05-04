83fb51cfc70a2379120ea446c03a3def337707cd
kitdea <francismarrosales@gmail.com>
2021-05-04 17:29:25 +0800

    reset js

--- public/app.js
+++ public/app.js
@@ -1,30 +1,6 @@
 const form = document.querySelector('#tdl-task-form');
-const taskList = document.querySelector('#tdl-task-list')
+const taskList = document.querySelector('#tdl-task-list');
+const clearTask = document.querySelector('.tdl-clear-tasks');
 const taskInput = document.querySelector('#tdl-task-input');
-const addBtn = document.querySelector('#tdl-add-btn');
 
-loadEventListeners();
-
-function loadEventListeners() {
-    form.addEventListener('tdl-add-btn', addTask);
-};
-
-function addTask() {
-    if(taskInput.value === "") {
-        alert("Add a Task!");
-    }
-
-    const li = document.createElement('li');
-    li.className = 'tdl-task-list-item';
-    li.appendChild(document.createTextNode(taskInput.value));
-
-    const link = document.createElement('a');
-    link.className = 'delete-item';
-    link.innerHTML = '<i class="fas fa-trash-alt delete-icon flex-shrink-0 h-6 w-6 text-red-400"></i>';
-    li.appendChild(link);
-
-    
-
-    e.preventDefault();
-};
 
