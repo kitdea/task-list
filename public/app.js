b85eeeb7b4581f98ceaa38d77627a16be90be100
kitdea <francismarrosales@gmail.com>
2021-05-04 20:41:47 +0800

    dayend commit

--- public/app.js
+++ public/app.js
@@ -14,20 +14,21 @@ function addTask() {
       return false;
   }
     const li = document.createElement('li');
-    li.className = 'tdl-task-list-item tdl-task-list-item flex items-center place-items-center justify-between align-middle hover:bg-indigo-100 px-6 py-2 hover:shadow';
-    li.appendChild(document.createTextNode(taskInput.value));
-    const link = document.createElement('a');
-    link.className = 'delete-icon';
-    link.innerHTML = '<i class="fas fa-trash-alt delete-icon flex-shrink-0 h-6 w-6 text-red-500"></i>';
-    li.appendChild(link);
-    taskList.appendChild(li);
-    taskInput.value = '';
+        li.className = 'tdl-task-list-item tdl-task-list-item flex items-center place-items-center justify-between align-middle hover:bg-indigo-100 px-6 py-2 hover:shadow';
+        li.appendChild(document.createTextNode(taskInput.value));
+        const link = document.createElement('a');
+        link.className = 'delete-icon';
+        link.innerHTML = '<i class="fas fa-trash-alt delete-icon flex-shrink-0 h-6 w-6 text-red-500 cursor-pointer"></i>';
+        li.appendChild(link);
+        taskList.appendChild(li);
+        taskInput.value = '';
     console.log(li);
 }
 
 function removeTask() {
-    document.getElementById('#tdl-task-list-item').remove();
-    console.log(removeTask);
+    let li = document.getElementById('#tdl-task-list-item');
+    li.remove();
+    console.log(li);
 }
   
 
